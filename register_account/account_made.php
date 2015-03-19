df7e9c9983e5d8d535161f6d460c2e638ab1bd54
s. samiuddin <phpdevsami@gmail.com>
2015-03-19 22:11:38 +0000

    Update account_made.php

--- register_account/account_made.php
+++ register_account/account_made.php
@@ -1,6 +1,6 @@
 
 <?PHP
-	/* Copyright (c)  2015  S.Samiuddin.
+	/* Copyright (c)  2015  S.Samiuddin. phpdevsami@gmail.com
 	Permission is granted to copy, distribute and/or modify this document
 	under the terms of the GNU Free Documentation License, Version 1.2
 	or any later version published by the Free Software Foundation;
