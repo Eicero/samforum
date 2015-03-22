a07500238893abfb8ba0f8da12fe0f5588ca4fa5
phpdevsami <phpdevsami@gmail.com>
2015-03-22 14:01:20 +0000

    added copyright

--- search.php
+++ search.php
@@ -1,5 +1,13 @@
+<?PHP
+	/* Copyright (c)  2015  S.Samiuddin. phpdevsami@gmail.com
+Permission is granted to copy, distribute and/or modify this document
+under the terms of the GNU Free Documentation License, Version 1.2
+or any later version published by the Free Software Foundation;
+with no Invariant Sections, no Front-Cover Texts, and no Back-Cover
+Texts.  A copy of the license is included in the section entitled "GNU
+Free Documentation License". */
+?>
 
-	
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="GET">
   <input name="search_query" type="text"> 
   <input name="search" type="submit" value="Search">
